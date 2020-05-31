c1c3404efa2fde7d4592497e22c4a1b0610bddfc
cesargonzalezarias <47632249+cesargonzalezarias@users.noreply.github.com>
2020-05-31 14:07:32 -0400

    Add files via upload

---
+++ assoofs.c
@@ -0,0 +1,235 @@
+#include <linux/module.h>       /* Needed by all modules */
+#include <linux/kernel.h>       /* Needed for KERN_INFO  */
+#include <linux/init.h>         /* Needed for the macros */
+#include <linux/fs.h>           /* libfs stuff           */
+#include <linux/buffer_head.h>  /* buffer_head           */
+#include <linux/slab.h>         /* kmem_cache            */
+#include "assoofs.h"
+
+/*
+ *  Operaciones sobre ficheros
+ */
+ssize_t assoofs_read(struct file * filp, char __user * buf, size_t len, loff_t * ppos);
+ssize_t assoofs_write(struct file * filp, const char __user * buf, size_t len, loff_t * ppos);
+const struct file_operations assoofs_file_operations = {
+    .read = assoofs_read,
+    .write = assoofs_write,
+};
+
+ssize_t assoofs_read(struct file * filp, char __user * buf, size_t len, loff_t * ppos) {
+    printk(KERN_INFO "Read request\n");
+    return 0;
+}
+
+ssize_t assoofs_write(struct file * filp, const char __user * buf, size_t len, loff_t * ppos) {
+    printk(KERN_INFO "Write request\n");
+    return 0;
+}
+
+/*
+ *  Operaciones sobre directorios
+ */
+static int assoofs_iterate(struct file *filp, struct dir_context *ctx);
+const struct file_operations assoofs_dir_operations = {
+    .owner = THIS_MODULE,
+    .iterate = assoofs_iterate,
+};
+
+/*
+ *  Operaciones sobre inodos
+ */
+static int assoofs_create(struct inode *dir, struct dentry *dentry, umode_t mode, bool excl);
+struct dentry *assoofs_lookup(struct inode *parent_inode, struct dentry *child_dentry, unsigned int flags);
+static int assoofs_mkdir(struct inode *dir, struct dentry *dentry, umode_t mode);
+static struct inode_operations assoofs_inode_ops = {
+    .create = assoofs_create,
+    .lookup = assoofs_lookup,
+    .mkdir = assoofs_mkdir,
+};
+
+struct assoofs_inode_info *assoofs_get_inode_info(struct super_block *sb, uint64_t inode_no){
+	struct assoofs_inode_info *inode_info = NULL;
+	struct buffer_head *bh;
+	bh = sb_bread(sb, ASSOOFS_INODESTORE_BLOCK_NUMBER);
+	inode_info = (struct assoofs_inode_info *)bh->b_data;
+
+	struct assoofs_super_block_info *afs_sb = sb->s_fs_info;
+	struct assoofs_inode_info *buffer = NULL;
+	int i;
+	for (i = 0; i < afs_sb->inodes_count; i++) {
+		if (inode_info->inode_no == inode_no) {
+		buffer = kmalloc(sizeof(struct assoofs_inode_info), GFP_KERNEL);
+		memcpy(buffer, inode_info, sizeof(*buffer));
+		break;
+		}
+		inode_info++;
+	}
+
+	brelse(bh);
+	return buffer;
+}
+
+static struct inode *assoofs_get_inode(struct super_block *sb, int ino){
+	struct inode *inode;
+	struct assoofs_inode_info *inode_info;
+
+	inode_info = assoofs_get_inode_info(sb, ino);
+
+	inode = new_inode(sb);
+	inode->i_ino = ino;
+	inode->i_sb = sb;
+	inode->i_op = &assoofs_inode_ops;
+
+	if (S_ISDIR(inode_info->mode))
+		inode->i_fop = &assoofs_dir_operations;
+	else if (S_ISREG(inode_info->mode))
+		inode->i_fop = &assoofs_file_operations;
+	else
+		printk(KERN_ERR "Unknown inode type. Neither a directory nor a file.");
+
+	inode->i_atime = inode->i_mtime = inode->i_ctime = current_time(inode);
+	inode->i_private = inode_info;
+
+	return inode;
+}
+
+
+
+static int assoofs_iterate(struct file *filp, struct dir_context *ctx) {
+    printk(KERN_INFO "Iterate request\n");
+    return 0;
+}
+
+struct dentry *assoofs_lookup(struct inode *parent_inode, struct dentry *child_dentry, unsigned int flags) {
+    printk(KERN_INFO "Lookup request\n");
+    
+    // acceder al bloque de disco con el contenido del directorio apuntado por parent_inode
+    struct assoofs_inode_info *parent_info = parent_inode->i_private;
+	struct super_block *sb = parent_inode->i_sb;
+	struct buffer_head *bh;
+	int i;
+	bh = sb_bread(sb, parent_info->data_block_number);
+
+	// recorrer el contenido del directorio buscando la entrada cuyo nombre 
+	// se corresponda con el que buscamos. Si se localiza
+	// la entrada, entonces tenemos construir el inodo correspondiente.
+	struct assoofs_dir_record_entry *record;
+	record = (struct assoofs_dir_record_entry *)bh->b_data;
+	for (i=0; i < parent_info->dir_children_count; i++) {
+		if (!strcmp(record->filename, child_dentry->d_name.name)) {
+			struct inode *inode = assoofs_get_inode(sb, record->inode_no); // Funcion auxiliar que obtine la informacion de un inodo a partir de su numero de inodo.
+			inode_init_owner(inode, parent_inode, ((struct assoofs_inode_info *)inode->i_private)->mode);
+			d_add(child_dentry, inode);
+			return NULL;
+		}
+		record++;
+	}
+    return NULL;
+}
+
+
+static int assoofs_create(struct inode *dir, struct dentry *dentry, umode_t mode, bool excl) {
+    printk(KERN_INFO "New file request\n");
+    return 0;
+}
+
+static int assoofs_mkdir(struct inode *dir , struct dentry *dentry, umode_t mode) {
+    printk(KERN_INFO "New directory request\n");
+    return 0;
+}
+
+/*
+ *  Operaciones sobre el superbloque
+ */
+static const struct super_operations assoofs_sops = {
+    .drop_inode = generic_delete_inode,
+};
+
+/*
+ *  Inicialización del superbloque
+ */
+int assoofs_fill_super(struct super_block *sb, void *data, int silent) {   
+    printk(KERN_INFO "assoofs_fill_super request\n");
+    // 1.- Leer la información persistente del superbloque del dispositivo de bloques
+    
+    struct buffer_head *bh;
+	struct assoofs_super_block_info *assoofs_sb;
+	
+	bh = sb_bread(sb, ASSOOFS_SUPERBLOCK_BLOCK_NUMBER); // sb lo recibe assoofs_fill_super como argumento
+	assoofs_sb = (struct assoofs_super_block_info *)bh->b_data;
+    
+    // 2.- Comprobar los parámetros del superbloque
+
+    if(assoofs_sb->magic != 0x20200406){
+    	return -1;
+    }
+    if(assoofs_sb->block_size != 4096){
+    	return -1;
+    }
+
+    // 3.- Escribir la información persistente leída del dispositivo de bloques en el superbloque sb, incluído el campo s_op con las operaciones que soporta.
+    
+    sb->s_magic = assoofs_sb->magic;
+    sb->s_maxbytes = assoofs_sb->block_size;
+    sb->s_op = &assoofs_sops;
+    sb->s_fs_info = assoofs_sb;
+
+    // 4.- Crear el inodo raíz y asignarle operaciones sobre inodos (i_op) y sobre directorios (i_fop)
+
+    struct inode *root_inode;
+    root_inode = new_inode(sb);
+    inode_init_owner(root_inode, NULL, S_IFDIR); // S_IFDIR para directorios, S_IFREG para ficheros.
+
+    root_inode->i_ino = ASSOOFS_ROOTDIR_INODE_NUMBER; // numero de inodo
+	root_inode->i_sb = sb; // puntero al superbloque
+	root_inode->i_op = &assoofs_inode_ops; // direccion de una variable de tipo struct inode_operations previamente declarada
+	root_inode->i_fop = &assoofs_dir_operations; // direccion de una variable de tipo struct file_operations previamente declarada.
+	root_inode->i_atime = root_inode->i_mtime = root_inode->i_ctime = current_time(root_inode); // fechas.
+	root_inode->i_private = assoofs_get_inode_info(sb, ASSOOFS_ROOTDIR_INODE_NUMBER); // Informacion persistente del inodo
+
+	sb->s_root = d_make_root(root_inode);
+	if(!sb->s_root){
+		brelse(bh);
+		return -1;
+	}
+
+	brelse(bh); // liberar memoria despues de utilizar el bloque
+    return 0;
+}
+
+/*
+ *  Montaje de dispositivos assoofs
+ */
+static struct dentry *assoofs_mount(struct file_system_type *fs_type, int flags, const char *dev_name, void *data) {
+    printk(KERN_INFO "assoofs_mount request\n");
+    struct dentry *ret = mount_bdev(fs_type, flags, dev_name, data, assoofs_fill_super);
+    // Control de errores a partir del valor de ret. En este caso se puede utilizar la macro IS_ERR: if (IS_ERR(ret)) ...
+}
+
+/*
+ *  assoofs file system type
+ */
+static struct file_system_type assoofs_type = {
+    .owner   = THIS_MODULE,
+    .name    = "assoofs",
+    .mount   = assoofs_mount,
+    .kill_sb = kill_litter_super,
+};
+
+// registrar el nuevo sistema de ficheros en el kernel.
+static int __init assoofs_init(void) {
+    printk(KERN_INFO "assoofs_init request\n");
+    int ret = register_filesystem(&assoofs_type);
+    // Control de errores a partir del valor de ret
+}
+
+// eliminar la informaci ́on del nuevo sistema de ficheros del kernel.
+static void __exit assoofs_exit(void) {
+    printk(KERN_INFO "assoofs_exit request\n");
+    int ret = unregister_filesystem(&assoofs_type);
+    // Control de errores a partir del valor de ret
+}
+
+module_init(assoofs_init);
+module_exit(assoofs_exit);
+
